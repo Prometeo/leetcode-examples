a873f0037435d136f31f73534fa3701b1da50795
Prometeo <xairi.valdivia@gmail.com>
2025-03-07 21:27:43 -0500

    feat: Remove innecesari list creation

--- arrays/missing_numbers/solution.py
+++ arrays/missing_numbers/solution.py
@@ -1,10 +1,9 @@
 def find_missing_numbers(nums: list[int]) -> list:
     missing_numbers = []
     set_of_values = set(nums)
-    range_of_numbers = [i for i in range(1, len(nums) + 1)]
+    # range_of_numbers = [i for i in range(1, len(nums) + 1)]
 
-    if len(set_of_values) < len(range_of_numbers):
-        for i in range_of_numbers:
-            if i not in set_of_values:
-                missing_numbers.append(i)
+    for i in range(1, len(nums) + 1):
+        if i not in set_of_values:
+            missing_numbers.append(i)
     return missing_numbers
