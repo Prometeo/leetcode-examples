ad5c4eb77579947e693d939a488b28fd526cc49b
Prometeo <xairi.valdivia@gmail.com>
2025-03-13 17:49:31 -0500

    feat: spiral matrix

---
+++ arrays/spiral_matrix/solution.py
@@ -0,0 +1,16 @@
+def spiral_matrix1(matrix: list[list[int]]) -> list:
+    result = list()
+    while matrix:
+        result += matrix.pop(0)
+
+        if matrix and matrix[0]:
+            for row in matrix:
+                result.append(row.pop())
+
+        if matrix:
+            result += matrix.pop()[::-1]
+
+        if matrix and matrix[0]:
+            for row in matrix[::-1]:
+                result.append(row.pop(0))
+    return result
